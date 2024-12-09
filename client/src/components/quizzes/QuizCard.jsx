1744f637a9cc4f364c90c9a323c121df0d4e8deb
Mehdi <166039375+MehdiMahmoudico@users.noreply.github.com>
2024-12-09 18:17:56 +0100

    Update QuizCard.jsx

--- client/src/components/quizzes/QuizCard.jsx
+++ client/src/components/quizzes/QuizCard.jsx
@@ -60,7 +60,7 @@ const QuizCard = ({ name, description, level, questionsCount, image, id,onDelete
             <CardMedia
                 component="img"
                 height="140"
-                image={`http://localhost:8000/${image}`}
+                image={`http://107.22.157.99/${image}`}
                 alt={name}
                 sx={{ borderTopLeftRadius: "8px", borderTopRightRadius: "8px" }}
             />
